defe7b303510d5210ff6fc229ef970b4e2b8c3d1
Franklin <franklinmatos@gmail.com>
2018-02-07 22:15:45 -0300

    criacao de pasta para armazenamento de modelos

---
+++ app/Models/Balance.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franklin
+ * Date: 07/02/18
+ * Time: 22:07
+ */
+
+namespace App\Models;
+
+
+class Balance
+{
+
+}
---
+++ app/Models/Historic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franklin
+ * Date: 07/02/18
+ * Time: 22:09
+ */
+
+namespace App\Models;
+
+
+class Historic
+{
+
+}
