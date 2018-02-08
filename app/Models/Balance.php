adf71f826302692af58e0a1a09f1b361dbe9792b
franklinmatos <franklinmatos@gmail.com>
2018-02-08 14:47:37 -0300

    alteracoes nos modelos e criacao de mitrarion

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: franklin
- * Date: 07/02/18
- * Time: 22:07
- */
 
 namespace App\Models;
-
 
 use Illuminate\Database\Eloquent\Model;
 
 class Balance extends Model
 {
-
+    //
 }
--- app/Models/Historic.php
+++ app/Models/Historic.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: franklin
- * Date: 07/02/18
- * Time: 22:09
- */
 
 namespace App\Models;
-
 
 use Illuminate\Database\Eloquent\Model;
 
 class Historic extends Model
 {
-
+    //
 }
