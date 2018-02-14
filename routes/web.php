b3c9e7f2994ee523fa6642ad2309b7648637ca8d
Franklin <franklinmatos@gmail.com>
2018-02-14 13:08:27 -0200

    alteracoes no grupo para que funcionasse perfeitamente

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
-$this->group(['middleware'=>'auth'],['namespace'=> 'Admin'],function(){
+
+$this->group(['middleware'=>['auth'],'namespace'=> 'Admin'],function(){
     $this->get('admin', 'AdminController@index')->name('admin.home');
 
 });
 
-$this->get('/','Site\SiteController@index')->name('home');
 
+$this->get('/','Site\SiteController@index')->name('home');
 
 Auth::routes();
