0e71fe108f0176668c83e6cca28501f198aa45fa
Franklin <franklinmatos@gmail.com>
2018-02-23 20:48:02 -0300

    alteracoes para cadastro de saldo e gravacao de historico

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -10,16 +10,22 @@ class Balance extends Model
     public $timestamps = false;
 
     public function deposit(float $value){
+        $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.','');
-       $retorno =  $this->save();
+       $deposit =  $this->save();
 
-       if($retorno){
+       $historic = auth()->user()->historics()->create([
+           'type' => 'I',
+           'amount' => $value,
+           'total_before' => $totalBefore,
+           'total_after' => $this->amount,
+           'date' => date('Ymd'),
+       ]);
+       if($deposit && $historic){
            return [
                'success' => true,
                'message' => 'A Recarga foi efetuada com sucesso!'
            ];
-
-
            return [
                'success' => false,
                'message' => 'Ocorreu um erro ao tentar efetuar a recarga'
